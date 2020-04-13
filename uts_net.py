4bd614c8ee094b34551701bd0c23beacdf50556d
undrey <mail@undrey.com>
2020-04-13 17:56:55 +0300

    ищем подключения

--- uts_net.py
+++ uts_net.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
 import re
 
+# создаем файл с ip и названиями машин
 array = []
 with open("openvpn-status.log", "r") as my_file:
     raw_data = my_file.readlines()
@@ -15,3 +17,22 @@ array = sorted(array)
 
 with open("net.csv", "w") as my_file:
     my_file.writelines(array)
+
+with open("users.csv", "r") as my_users:
+    raw_data = my_users.readlines()
+print(raw_data)
+result = re.findall(r'\w+\s\w+\s\w+\,\w+\,\w+\-\w+\-\d+', str(raw_data))
+print(result)
+array2 = []
+for i in result:
+    array2.append(i.lower())
+array2 = sorted(array2)
+
+array3 = []
+for i in array2:
+    for y in array:
+        if y.split(',')[0] in i:
+            x = i + ',' + y.split(',')[1] + '\n'
+            array3.append(x)
+with open("net_users.csv", "w") as my_file:
+    my_file.writelines(array3)
