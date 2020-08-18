fe641cc2521f0050d23e718a050b616389fb1fb9
undrey <mail@undrey.com>
2020-08-18 16:01:10 +0300

    Sap Sync

--- sap_sync/mirror-sap.py
+++ sap_sync/mirror-sap.py
@@ -2,14 +2,14 @@ import shutil
 
 const_sync_file = '/home/undrey/1/now_sync.txt'
 prev_sync_file = '/home/undrey/1/prev_sync.txt'
-
+modif_sync_file = '/home/undrey/1/modified.txt'
 actual_dict = {}
 difs = []
 with open(const_sync_file, 'r') as f:
     for line in f:
         size, filename = line.split(' ')
         actual_dict[filename] = size
-print(actual_dict)
+
 
 with open(prev_sync_file, 'r') as f:
     for line in f:
@@ -17,6 +17,11 @@ with open(prev_sync_file, 'r') as f:
         if filename in actual_dict:
             if actual_dict[filename] != size:
                 print(actual_dict[filename], size)
-print(actual_dict)
+                difs.append(filename)
+
+with open(modif_sync_file, 'w') as f:
+    for dif in difs:
+        f.writelines(dif+'\n')
+
 
 shutil.copyfile(const_sync_file, prev_sync_file)
