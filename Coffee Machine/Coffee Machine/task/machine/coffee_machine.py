643944e8000e12d33e9df44d6361758e02cf2fe9
Undrey <mail@undrey.co>
2020-08-24 21:09:25 +0300

    Some code

--- Coffee Machine/Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/Coffee Machine/task/machine/coffee_machine.py
@@ -1,3 +1,20 @@
+# water = int(input('Write how many ml of water the coffee machine has:\n'))
+# milk = int(input('Write how many ml of milk the coffee machine has\n'))
+# beans = int(input('Write how many grams of coffee beans the coffee machine has:\n'))
+# cups = int(input('Write how many cups of coffee you will need:\n'))
+# water_need = int(water / 200)
+# milk_need = int(milk / 50)
+# beans_need = int(beans / 15)
+#
+# num_cups = min(water_need, milk_need, beans_need)
+#
+# if num_cups == cups:
+#     print('Yes, I can make that amount of coffee')
+# elif num_cups > cups:
+#     print('Yes, I can make that amount of coffee (and even ' + str(num_cups - cups) + ' more than that)')
+# else:
+#     print('No, I can make only ' + str(num_cups) + ' cups of coffee')
+
 class CoffeMaschine():
 
     def __init__(self):
@@ -77,3 +94,83 @@ class CoffeMaschine():
                 print("Wrong choice")
 machine=CoffeMaschine()
 machine.coffee_action()
+
+#
+# def print_machine():
+#     print(f'The coffee machine has:\n{water} of water\n{milk} of milk'
+#           f'\n{beans} of coffee beans\n{disposable_cups} of disposable cups\n'
+#           f'{money} of money\n')
+#
+#
+# def buy_cofe(choice):
+#     global water
+#     global milk
+#     global beans, disposable_cups, money
+#     if choice == '1':
+#         if water < 250 or beans < 6 or disposable_cups < 1:
+#             print('Sorry, not enough water!')
+#         else:
+#             water -= 250
+#             beans -= 16
+#             disposable_cups -= 1
+#             money += 4
+#             print('I have enough resources, making you a coffee!')
+#     elif choice == '2':
+#         if water < 350 or beans < 20 or milk < 75 or disposable_cups < 1:
+#             print('Sorry, not enough water!')
+#         else:
+#             water -= 350
+#             beans -= 20
+#             milk -= 75
+#             disposable_cups -= 1
+#             money += 7
+#             print('I have enough resources, making you a coffee!')
+#     elif choice == '3':
+#         if water < 200 or beans < 12 or milk < 100 or disposable_cups < 1:
+#             print('Sorry, not enough water!')
+#         else:
+#             water -= 200
+#             beans -= 12
+#             milk -= 100
+#             disposable_cups -= 1
+#             money += 6
+#             print('I have enough resources, making you a coffee!')
+#     else:
+#         print("Wrong choice!")
+#
+#
+# def fill_cofe():
+#     global water, milk, beans, disposable_cups, money
+#
+#     add_some = input('Write how many ml of water do you want to add:')
+#     water += int(add_some)
+#
+#     add_some = input('Write how many ml of milk do you want to add:')
+#     milk += int(add_some)
+#     add_some = input('Write how many grams of coffee beans do you want to add:')
+#     beans += int(add_some)
+#     add_some = input('Write how many disposable cups of coffee do you want to add:')
+#     disposable_cups += int(add_some)
+#
+#
+# def take_money():
+#     global money
+#     print('I gave you $' + str(money))
+#     money = 0
+#
+#
+# while True:
+#     choice = input('Write action (buy, fill, take, remaining, exit):')
+#     if choice == 'buy':
+#         buy_choice = input('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:')
+#         buy_cofe(buy_choice)
+#     elif choice == 'fill':
+#         fill_cofe()
+#     elif choice == 'take':
+#         take_money()
+#     elif choice == 'remaining':
+#         print_machine()
+#     elif choice == 'exit':
+#         break
+#     else:
+#         print("Wrong choice")
--- Delete Old Bases/delete_database_logs.py
+++ Delete Old Bases/delete_database_logs.py
@@ -2,7 +2,7 @@ import re
 import os
 import shutil
 
-PATH = 'C:\\Program Files\\1cv8\\srvinfo\\reg_1541_test'
+PATH = 'C:\\Program Files\\1cv8\\srvinfo\\reg_1541'
 os.chdir(PATH)
 
 # считываем список
@@ -13,5 +13,9 @@ database_names = re.findall(r'\w{8}-\w{4}-\w{4}-\w{4}-\w{12}', str(raw_data))
 # считываем директорию и удаляем лишнии папки
 files_list = re.findall(r'\w{8}-\w{4}-\w{4}-\w{4}-\w{12}', str(os.listdir()))
 for i in files_list:
-    if i not in database_names:
-        shutil.rmtree(i)
+    if i=='snccntxaa36abcb-4824-400c-9b73-bde43f1c83e3' or 'snccntx56b084d5-0467-429c-9830-227354e8f1f4':
+        pass
+    else:
+        if i not in database_names:
+            shutil.rmtree(i)
+            print(i)
